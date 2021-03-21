047c38565f8a78320ba0b236295c7399ecbf9f9a
flukekd <flukekd@gmail.com>
2021-03-22 02:40:10 +0700

    updated shield

---
+++ lb_cache with tcp download file/stockhome with shield/tcpcacheDB.go
@@ -0,0 +1,431 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var Db *sql.DB
+var myCache LRU
+
+func main() {
+	//ยังไม่รู้ค่าจริงของ init
+	myCache.InitLRU(10)
+	connect, err := net.Listen("tcp", ":5003")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer connect.Close()
+	for {
+		con, err := connect.Accept()
+		if err != nil {
+			fmt.Println(err)
+			// connect.Close()
+			return
+		}
+		go rec(con)
+		fmt.Println(con.RemoteAddr())
+		// go send(con, rec(con))
+	}
+}
+
+func rec(con net.Conn) {
+	for {
+		data, err := bufio.NewReader(con).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(data)
+		msg := strings.Split(data, ":")
+		msg[0] = strings.TrimSpace(msg[0])
+		switch msg[0] {
+		case "add":
+			msg[1] = strings.TrimSpace(msg[1])
+			id := strings.Split(msg[1], "-")
+			id[0] = strings.TrimSpace(id[0])
+			id[1] = strings.TrimSpace(id[1])
+			id[2] = strings.TrimSpace(id[2])
+			iid, err := strconv.Atoi(id[0])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			amt, err := strconv.Atoi(id[1])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			uid, err := strconv.Atoi(id[2])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			send(con, addToDB(iid, amt, uid))
+		case "wd":
+			msg[1] = strings.TrimSpace(msg[1])
+			id := strings.Split(msg[1], "-")
+			id[0] = strings.TrimSpace(id[0])
+			id[1] = strings.TrimSpace(id[1])
+			id[2] = strings.TrimSpace(id[2])
+			iid, err := strconv.Atoi(id[0])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			amt, err := strconv.Atoi(id[1])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			uid, err := strconv.Atoi(id[2])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			send(con, withDrawToDB(iid, amt, uid))
+		case "get":
+			msg[1] = strings.TrimSpace(msg[1])
+			iid, err := strconv.Atoi(msg[1])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			send(con, getAmountbyItem(iid))
+		default:
+			send(con, "DB Error!")
+		}
+	}
+}
+
+func send(con net.Conn, msg string) {
+	fmt.Println("msg:", msg)
+	con.Write([]byte("Database: " + msg))
+}
+
+func init() {
+	var err error
+	Db, err = sql.Open("mysql", "root:pinkponk@tcp(127.0.0.1:3306)/stockhome")
+
+	if err != nil {
+		fmt.Println("Error: Cannot open database")
+	}
+}
+
+var Wg sync.WaitGroup
+
+func Main(itemID int, amount int, userID int) string {
+	// defer Db.Close()
+	var statement string
+	// Wg := sync.WaitGroup{}
+
+	Wg.Add(1)
+	go func() {
+		statement = addNew(itemID, amount, userID, &Wg)
+	}()
+	Wg.Wait()
+	return statement
+}
+
+func Main2(itemID int, amount int, userID int) string {
+	// defer Db.Close()
+	var statement string
+	Wg := sync.WaitGroup{}
+
+	Wg.Add(1)
+	go func() {
+		statement = withdraw(itemID, amount, userID, &Wg)
+	}()
+	Wg.Wait()
+	return statement
+}
+
+func GetAmount(itemID int) int {
+	row, err := Db.Query("SELECT itemID, amount FROM stock WHERE itemID = (?)", itemID)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var amount int
+	for row.Next() {
+		err = row.Scan(&itemID, &amount)
+	}
+	return amount
+}
+
+func addNew(itemID int, amount int, userID int, Wg *sync.WaitGroup) string {
+	// For adding NEW items. For items NOT CURRENTLY in the database.
+	// If you add an existing item, it will die. Use addExist for items already in database
+	defer Wg.Done()
+	var checkID int
+	var statement string
+
+	check := Db.QueryRow("SELECT itemID FROM stock WHERE itemID = (?)", itemID).Scan(&checkID)
+
+	if check != nil {
+		insert, err := Db.Query("INSERT INTO stock VALUES (?, ?)", itemID, amount)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		statement = fmt.Sprint("Added %d to database (%d units) | Item in Stock: %d\n", itemID, amount, amount)
+		addHis(itemID, true, amount, userID)
+		insert.Close()
+
+	} else {
+		Wg.Add(1)
+		go func() {
+			addExist(itemID, amount, userID, Wg)
+		}()
+	}
+	return statement
+}
+
+func addExist(itemID int, amount int, userID int, Wg *sync.WaitGroup) string {
+	// For adding EXISTING items. For items CURRENTLY in the database.
+	// If you add a new item, it will die. Use addNew for items NOT in database
+	defer Wg.Done()
+	var checkID, stock int
+	var statement string
+
+	check := Db.QueryRow("SELECT itemID, amount FROM stock WHERE itemID = (?)", itemID).Scan(&checkID, &stock)
+
+	if check != nil {
+		fmt.Println("Error: Item does not exist in database")
+	} else {
+		add, err := Db.Query("UPDATE stock SET amount = (?) WHERE itemID = (?)", stock+amount, itemID)
+
+		if err != nil {
+			fmt.Println(err)
+			return "error happended in addExist"
+		}
+		statement = fmt.Sprintf("Added %d to database (%d units) | Item in Stock: %d\n", itemID, amount, stock+amount)
+		addHis(itemID, true, amount, userID)
+		add.Close()
+
+	}
+	return statement
+}
+
+func withdraw(itemID int, amount int, userID int, Wg *sync.WaitGroup) string {
+	defer Wg.Done()
+	var checkID, stock int
+	var statement string
+
+	check := Db.QueryRow("SELECT itemID, amount FROM stock WHERE itemID = (?)", itemID).Scan(&checkID, &stock)
+
+	if check != nil {
+		fmt.Println("Error: No item in stock")
+
+	} else if amount > stock {
+		fmt.Println("Error: Amount exceeds stock")
+
+	} else {
+		with, err := Db.Query("UPDATE stock SET amount = (?) WHERE itemID = (?)", stock-amount, itemID)
+
+		if err != nil {
+			fmt.Printf("\n")
+		}
+		statement = fmt.Sprintf("Withdrawn %d from database (%d units) | Item in Stock: %d\n", itemID, amount, stock-amount)
+		addHis(itemID, false, amount, userID)
+		with.Close()
+	}
+	return statement
+}
+
+func addHis(itemID int, action bool, amount int, userID int) {
+	// This already auto-adds itself to the history database, so no need to do anything here.
+	var datetime = time.Now()
+	date := datetime.Format("2006-01-02")
+	time := datetime.Format("15:04:05")
+	fmt.Println("hi", action, userID, itemID, amount, date, time)
+	add, err := Db.Query("INSERT INTO history (action, userID, itemID, amount, date, time) VALUES(?, ?, ?, ?, ?, ?)", action, userID, itemID, amount, date, time)
+
+	if err != nil {
+		fmt.Println("Error: Cannot be added to history")
+	}
+
+	add.Close()
+}
+
+//จบ DB
+
+//เริ่ม cache
+
+var i int
+var dateAndTime time.Time = time.Now()
+
+type cache struct {
+	//4
+	itemID        int
+	currentAmount int
+	Date          string
+	Time          string
+	prev, next    *cache
+}
+
+//มาแก้
+func addcache(itemID int, ItemAmount int) *cache {
+	return &cache{
+		itemID:        itemID,
+		currentAmount: ItemAmount,
+		Date:          dateAndTime.Format("2006-01-02"),
+		Time:          dateAndTime.Format("15:04:05 2006-01-02")[:8],
+		prev:          nil,
+		next:          nil,
+	}
+}
+
+type queue struct {
+	front *cache
+	rear  *cache
+}
+
+func (q *queue) isEmpty() bool {
+	return q.rear == nil
+}
+
+func (q *queue) addFrontPage(itemID int, ItemAmount int) *cache {
+	page := addcache(itemID, ItemAmount)
+	if q.front == nil && q.rear == nil {
+		q.front, q.rear = page, page
+	} else {
+		page.next = q.front.next
+		q.front.prev = page
+		q.front = page
+	}
+	return page
+}
+
+func (q *queue) bringToMostUsed(page *cache) {
+	if page == q.front {
+		return
+	} else if page == q.rear {
+		q.rear = q.rear.prev
+		q.rear.next = nil
+	} else {
+		page.prev.next = page.next
+		page.next.prev = page.prev
+	}
+	page.next = q.front
+	q.front.prev = page
+	q.front = page
+}
+
+func (q *queue) removeLeastUsed() {
+	if q.isEmpty() {
+		return
+	} else if q.front == q.rear {
+		q.front, q.rear = nil, nil
+	} else {
+		q.rear = q.rear.prev
+		q.rear.next = nil
+	}
+}
+
+func (q *queue) getRear() *cache {
+	return q.rear
+}
+
+type LRU struct {
+	capacity, size int
+	pageList       queue
+	PageMap        map[int]*cache
+}
+
+func (l *LRU) InitLRU(capacity int) {
+	l.capacity = capacity
+	l.PageMap = make(map[int]*cache)
+}
+
+func (l *LRU) Read(itemID int) int {
+	if _, found := l.PageMap[itemID]; !found {
+		return GetAmount(itemID)
+	}
+	val := l.PageMap[itemID].currentAmount
+	l.pageList.bringToMostUsed(l.PageMap[itemID])
+	return val
+}
+
+func (l *LRU) Input(itemID int, ItemAmount int) int {
+	if _, found := l.PageMap[itemID]; found {
+		l.PageMap[itemID].currentAmount = l.PageMap[itemID].currentAmount + ItemAmount
+		l.pageList.bringToMostUsed(l.PageMap[itemID])
+		return 0
+	}
+	if l.size == l.capacity {
+		key := l.pageList.getRear().itemID
+		l.pageList.removeLeastUsed()
+		l.size--
+		delete(l.PageMap, key)
+	}
+	if ItemAmount < 0 {
+		if GetAmount(itemID)+ItemAmount < 0 {
+			fmt.Print("ItemID: %#v  cannot be withdraw!!, Negative Value", itemID)
+			return -1
+		} else {
+			page := l.pageList.addFrontPage(itemID, GetAmount(itemID)+ItemAmount)
+			l.size++
+			l.PageMap[itemID] = page
+		}
+	}
+
+	if ItemAmount > 0 {
+		page := l.pageList.addFrontPage(itemID, GetAmount(itemID)+ItemAmount)
+		l.size++
+		l.PageMap[itemID] = page
+	}
+	return 0
+}
+
+// func main() {
+// 	var cache LRU
+// 	cache.InitLRU(10)
+
+// }
+
+//getItemAmount จาก TCP request
+func getAmountbyItem(itemID int) string {
+	amount := myCache.Read(itemID)
+	itemid := strconv.Itoa(itemID)
+	result := strconv.Itoa(amount)
+	fmt.Println(itemid + "-" + result + "\n")
+	return (itemid + "-" + result + "\n")
+}
+
+// add() request
+func addToDB(itemID int, amount int, userID int) string {
+	myCache.Input(itemID, amount)
+	statement := Main(itemID, amount, userID)
+	// itemid := strconv.Itoa(itemID)
+	// result := strconv.Itoa(amount)
+	fmt.Println(statement + "\n")
+	return "Success\n"
+}
+
+//withdraw() tcp
+//withdraw()database จาก server
+func withDrawToDB(itemID int, amount int, userID int) string {
+	err := myCache.Input(itemID, amount)
+	if err == -1 {
+		// return error ให้ users
+		return "cannot withdraw, Database got negative amount."
+	}
+	statement := Main2(itemID, amount, userID)
+	// itemid := strconv.Itoa(itemID)
+	// result := strconv.Itoa(amount)
+	fmt.Println(statement + "\n")
+	return "Success\n"
+}
+
+//ถ้าจะรัน cache ใหม่ต่อวันต้อง while True init ใหม่
---
+++ lb_cache with tcp download file/stockhome with shield/tcpclient.go
@@ -0,0 +1,300 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	con, err := net.Dial("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer con.Close()
+	help()
+	for {
+		fmt.Println("Command: ")
+		msg, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			return
+		}
+		com := strings.Split(msg, " ")
+		com[0] = strings.TrimSpace(com[0])
+		switch com[0] {
+		case "add":
+			add(con, com)
+		case "wd":
+			wd(con, com)
+		case "his":
+			his(con, com)
+		case "ana":
+			ana(con, com)
+		case "help":
+			help()
+		case "exit":
+			con.Close()
+			return
+		case "get":
+			get(con, com)
+		default:
+			fmt.Println("Command not found. Type \"help\" for help.")
+		}
+	}
+
+}
+
+func help() {
+	fmt.Println(" Features 		|\"Command\"						|\"Example\"")
+	fmt.Println(" ---------------------------------------------------------------------------------------------- ")
+	fmt.Println(" Add Item		|\"add userID itemID Amount\"				|\"add 62011155 745345 12\"")
+	fmt.Println(" WithDraw Item		|\"wd userID itemID Amount\"				|\"wd 62011155 745345 12\"")
+	fmt.Println(" History Tracking	|\"his year-month\"	|\"his 2020-12\"")
+	fmt.Println(" Stock Analysis 	|\"ana year-month-day\"				|\"ana 2020-12-12\"")
+	fmt.Println(" Exit 			|\"exit\"")
+}
+
+func add(con net.Conn, com []string) { //add userid itemid amount
+	if len(com) < 4 {
+		fmt.Println("Not Enough Information.")
+		return
+	}
+	uid, err := strconv.Atoi(com[1])
+	if err != nil {
+		fmt.Println("Please Enter an Integer!")
+		return
+	}
+	iid, err := strconv.Atoi(com[2])
+	if err != nil {
+		fmt.Println("Please Enter an Integer!")
+		return
+	}
+	com[3] = strings.TrimSpace(com[3])
+	amt, err := strconv.Atoi(com[3])
+	if err != nil {
+		fmt.Println("Please Enter an Integer!")
+		return
+	}
+	if uid <= 0 {
+		fmt.Println("UserID cannot be zero or negative.")
+		return
+	}
+	if iid <= 0 {
+		fmt.Println("ItemID cannot be zero or negative.")
+		return
+	}
+	if amt <= 0 {
+		fmt.Println("Amount cannot be zero or negative.")
+		return
+	}
+	con.Write([]byte(com[0] + ": " + com[1] + "-" + com[2] + "-" + com[3] + "\n"))
+	fmt.Println("Waiting for respond...")
+	data, err := bufio.NewReader(con).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(data)
+}
+
+func wd(con net.Conn, com []string) {
+	if len(com) != 4 {
+		fmt.Println("Please input as the format.")
+		return
+	}
+	uid, err := strconv.Atoi(com[1])
+	if err != nil {
+		fmt.Println("Please Enter an Integer.")
+		return
+	}
+	iid, err := strconv.Atoi(com[2])
+	if err != nil {
+		fmt.Println("Please Enter an Integer.")
+		return
+	}
+	com[3] = strings.TrimSpace(com[3])
+	amt, err := strconv.Atoi(com[3])
+	if err != nil {
+		fmt.Println("Please Enter an Integer.")
+		return
+	}
+	if uid <= 0 {
+		fmt.Println("UserID cannot be zero or negative.")
+		return
+	}
+	if iid <= 0 {
+		fmt.Println("ItemID cannot be zero or negative.")
+		return
+	}
+	if amt <= 0 {
+		fmt.Println("Amount cannot be zero or negative.")
+		return
+	}
+	con.Write([]byte(com[0] + ": " + com[1] + "-" + com[2] + "-" + com[3] + "\n"))
+	fmt.Println("Waiting for respond...")
+	data, err := bufio.NewReader(con).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(data)
+}
+
+func his(con net.Conn, com []string) {
+	if len(com) != 2 {
+		fmt.Println("Please input as the format.")
+		return
+	}
+	since := strings.Split(com[1], "-")
+	if len(since) != 2 {
+		fmt.Println("Please input as the format.")
+		return
+	}
+
+	yyyy, err := strconv.Atoi(since[0])
+	if err != nil {
+		fmt.Println("Please Enter year as an Integer!")
+		return
+	}
+	if len(since[0]) != 4 {
+		fmt.Println("Please Enter year as a 4 digits of int!")
+		return
+	}
+	if yyyy > time.Now().Year() {
+		fmt.Println("Cannot diplay the future!")
+		return
+	}
+
+	since[1] = strings.TrimSpace(since[1])
+
+	mm, err := strconv.Atoi(since[1])
+	if err != nil {
+		fmt.Println("Please Enter month as an Integer!")
+		return
+	}
+	if len(since[1]) != 2 {
+		fmt.Println("Please Enter year as a 2 digits of int!")
+		return
+	}
+	mmt := time.Now().Month()
+	var immt int = int(mmt)
+	if mm > immt && yyyy == time.Now().Year() {
+		fmt.Println("Cannot diplay the future!")
+		return
+	} else if mm == immt && yyyy == time.Now().Year() {
+		fmt.Println("Cannot diplay the current month!")
+		return
+	}
+
+	con.Write([]byte(com[0] + ": " + since[0] + since[1] + "\n"))
+
+	fmt.Println("Downloading...")
+
+	// Create a file that the client wants to download
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	out, err := os.Create(dir + "/" + since[0] + "-" + since[1] + ".tmp")
+	if err != nil {
+		return
+	}
+
+	// Receive data and writing the file
+	data, err := bufio.NewReader(con).ReadString('.')
+	out.Write([]byte(data))
+	out.Close()
+
+	// Rename temporary to acutal csv file
+	err = os.Rename(dir+"/"+since[0]+"-"+since[1]+".tmp", dir+"/"+since[0]+"-"+since[1]+".csv")
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Download completed")
+	return
+}
+
+func ana(con net.Conn, com []string) {
+	if len(com) != 2 {
+		fmt.Println("Please input as the format.")
+		return
+	}
+	since := strings.Split(com[1], "-")
+	if len(since) != 3 {
+		fmt.Println("Please input as the format.")
+		return
+	}
+
+	yyyy, err := strconv.Atoi(since[0])
+	if err != nil {
+		fmt.Println("Please Enter year as an Integer!")
+		return
+	}
+	if len(since[0]) != 4 {
+		fmt.Println("Please Enter year as a 4 digits of int!")
+		return
+	}
+	if yyyy > time.Now().Year() {
+		fmt.Println("Cannot diplay the future!")
+		return
+	}
+
+	mm, err := strconv.Atoi(since[1])
+	if err != nil {
+		fmt.Println("Please Enter month as an Integer!")
+		return
+	}
+	if len(since[1]) != 2 {
+		fmt.Println("Please Enter year as a 2 digits of int!")
+		return
+	}
+	mmt := time.Now().Month()
+	var immt int = int(mmt)
+	if mm > immt && yyyy == time.Now().Year() {
+		fmt.Println("Cannot diplay the future!")
+		return
+	}
+
+	since[2] = strings.TrimSpace(since[2])
+	dd, err := strconv.Atoi(since[2])
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Please Enter day as an Integer!")
+		return
+	}
+	if len(since[2]) != 2 {
+		fmt.Println("Please Enter day as a 2 digits of int!")
+		return
+	}
+	if dd > time.Now().Day() && mm == immt && yyyy == time.Now().Year() {
+		fmt.Println("Cannot diplay the future!")
+		return
+	}
+
+	con.Write([]byte(com[0] + ": " + since[0] + "-" + since[1] + "-" + since[2] + "\n"))
+	fmt.Println("Waiting for respond...")
+	data, err := bufio.NewReader(con).ReadString('.')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(data)
+}
+
+func get(con net.Conn, com []string) {
+	con.Write([]byte(com[0] + ": " + com[1] + "\n"))
+	fmt.Println("Waiting for respond...")
+	data, err := bufio.NewReader(con).ReadString('.')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(data)
+}
