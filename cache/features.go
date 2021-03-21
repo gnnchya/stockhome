f5009a0f746771419f719b833e6b7fe417c1967e
thachawit <43896511+thachawit@users.noreply.github.com>
2021-03-21 21:48:34 +0700

    shield update

---
+++ cache/features.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	a "add_withdraw/cache"
+	b "add_withdraw/cache/db"
+	"fmt"
+)
+
+func add(itemID int, itemAmount int, userID int) {
+	return 
+}
+
+func withdraw(itemID int, itemAmount int, userID int){
+	return
+}
+
+func getItemAmout(itemID int){
+	return 
+}
