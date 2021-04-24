17ee3a2a7e010d5fa71a495940cdd596373f0afe
flukekd <flukekd@gmail.com>
2021-04-25 00:06:21 +0700

    Create baseline_history.go

---
+++ baseline_history.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main(filename string) {
+	Date := filename[0:4] + "-" + filename[4:6]
+	buf := bytes.NewBuffer(make([]byte, 0))
+	col := []byte("userID,itemID,amount,date,time")
+	buf.Write(col)
+	startDate := Date + "-01" //2021-02-01
+	endDate := Date + "-31"   //2021-02-31
+
+	row, err := db.Query("SELECT userID, itemID, amount, date, time FROM history WHERE date BETWEEN (?) AND (?) ORDER BY date ASC, time ASC", startDate, endDate)
+	if err != nil {
+		fmt.Print(err)
+	}
+	// Slice each row
+	for row.Next() {
+		var userID, itemID, amount int
+		var date, time string
+		err = row.Scan(&userID, &itemID, &amount, &date, &time)
+		if err != nil {
+			fmt.Print(err)
+		}
+		// Write each line
+		line := []byte("\n" + strconv.Itoa(userID) + "," + strconv.Itoa(itemID) + "," + strconv.Itoa(amount) + "," + date + "," + time)
+		buf.Write(line)
+	}
+	row.Close()
+
+	// Data
+	buf.Bytes()
+}
