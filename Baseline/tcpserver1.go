035bd2a676a1840efae585381d1f571134a92449
pondnarawich <pondnarawich@gmail.com>
2021-04-25 13:01:39 +0700

    add baseline and update wg in server1

--- Baseline/tcpserver1.go
+++ Baseline/tcpserver1.go
@@ -2,26 +2,17 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"database/sql"
 	"fmt"
 	"net"
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-var wgadd sync.WaitGroup
-var wgdb sync.WaitGroup
-var wgana sync.WaitGroup
-var wgwd sync.WaitGroup
-var wgget sync.WaitGroup
-
-// var wgexit sync.WaitGroup
-// var wgall sync.WaitGroup
 
 func main() {
 	connect, err := net.Listen("tcp", "128.199.70.252:5001")
@@ -40,18 +31,15 @@ func main() {
 		con, err := connect.Accept()
 		if err != nil {
 			fmt.Println(err)
-			// connect.Close()
 			return
 		}
 		go rec(con)
 		fmt.Println(con.RemoteAddr())
-		// go send(con, rec(con))
 	}
 }
 
 func rec(con net.Conn) {
 	for {
-		// wgall.Add(1)
 		data, err := bufio.NewReader(con).ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
@@ -65,61 +53,37 @@ func rec(con net.Conn) {
 		msg[1] = strings.TrimSpace(msg[1])
 		switch msg[0] {
 		case "ana":
-			// wgana.Add(1)
 			date := strings.Split(msg[1], "-")
 			date[0] = strings.TrimSpace(date[0])
 			date[1] = strings.TrimSpace(date[1])
 			date[2] = strings.TrimSpace(date[2])
-			wgana.Wait()
-			ana := analysis(date[0], date[1], date[2])
-			send(con, ana)
-			// wgana.Done()
+			send(con, analysis(date[0], date[1], date[2]))
+		case "his":
+			date := strings.Split(msg[1], "-")
+			date[0] = strings.TrimSpace(date[0])
+			date[1] = strings.TrimSpace(date[1])
+			sendhis(con, his(date[0], date[1]))
 		case "add":
-			// wgadd.Add(1)
 			id := strings.Split(msg[1], "-")
 			id[0] = strings.TrimSpace(id[0])
 			id[1] = strings.TrimSpace(id[1])
 			id[2] = strings.TrimSpace(id[2])
-			wgadd.Wait()
-			add := add(id[0], id[1], id[2])
-			send(con, add)
-			// wgadd.Done()
+			send(con, add(id[0], id[1], id[2]))
 		case "wd":
-			// wgwd.Add(1)
 			id := strings.Split(msg[1], "-")
 			id[0] = strings.TrimSpace(id[0])
 			id[1] = strings.TrimSpace(id[1])
 			id[2] = strings.TrimSpace(id[2])
-			wgwd.Wait()
-			wd := withdraw(id[0], id[1], id[2])
-			send(con, wd)
-			// wgwd.Done()
-		case "db":
-			// wgdb.Add(1)
-			wgdb.Wait()
-			pulldb(con, msg[1])
-			// wgdb.Done()
+			send(con, withdraw(id[0], id[1], id[2]))
 		case "get":
-			// wgget.Add(1)
-			wgget.Wait()
-			get := getItemAmount(msg[1])
-			send(con, get)
-			// wgget.Done()
+			send(con, getItemAmount(msg[1]))
 		case "exit":
-			// wgexit.Add(1)
 			con.Close()
 			fmt.Println("EOF")
-			// wgexit.Done()
 			return
 		default:
-			// wgall.Add(1)
 			send(con, "Some How Error!")
-			// wgall.Done()
 		}
-		// wgall.Done()
-
-		// wgexit.Wait()
-
 	}
 }
 
@@ -128,53 +92,30 @@ func send(con net.Conn, msg string) {
 
 }
 
+func sendhis(con net.Conn, msg []byte) {
+	temp := append([]byte("Server: "), msg...)
+	temp1 := append(temp, []byte(".")...)
+	con.Write(temp1)
+
+}
+
 var db *sql.DB
 
 func analysis(year string, month string, day string) string {
-	wgana.Add(1)
-	defer wgana.Done()
-	// var err error
-	// db, err = sql.Open("mysql", "root:pinkponk@tcp(209.97.170.50:3306)/stockhome")
-	// if err != nil {
-	// 	fmt.Println("Error: Cannot open database")
-	// }
-
-	// defer db.Close()
-
 	var start string = year + "-" + month + "-" + day
 	var aWith, bWith, cWith, dWith string
 
-	Wg := sync.WaitGroup{}
-
-	Wg.Add(1)
-	go func() {
-		aWith = MostWithA(&Wg)
-	}()
-
-	Wg.Add(1)
-	go func() {
-		bWith = MostWithDate(start, &Wg)
-	}()
-
-	Wg.Add(1)
-	go func() {
-		cWith = WithTime(&Wg)
-	}()
-
-	Wg.Add(1)
-	go func() {
-		dWith = WithDate(&Wg)
-	}()
-
-	Wg.Wait()
+	aWith = MostWithA()
+	bWith = MostWithDate(start)
+	cWith = WithTime()
+	dWith = WithDate()
 
 	return (aWith + "\n" + bWith + "\n" + cWith + "\n" + dWith + ".")
 }
 
-func MostWithA(Wg *sync.WaitGroup) string {
-	defer Wg.Done()
+func MostWithA() string {
 	var txt strings.Builder
-	row, err := db.Query("SELECT itemID, amount FROM history WHERE action = 0")
+	row, err := db.Query("SELECT itemID, amount FROM stockhome.history WHERE action = 0")
 
 	if err != nil {
 		fmt.Print(err)
@@ -219,14 +160,13 @@ func MostWithA(Wg *sync.WaitGroup) string {
 	return txt.String()
 }
 
-func MostWithDate(start string, Wg *sync.WaitGroup) string {
-	defer Wg.Done()
+func MostWithDate(start string) string {
 	var txt strings.Builder
 	startDate, _ := time.Parse("2006-01-02", start)
 	var end = time.Now()
 	endDate := end.Format("2006-01-02")
 
-	row, err := db.Query("SELECT itemID, amount FROM history WHERE action = 0 AND date BETWEEN (?) AND (?)", startDate, endDate)
+	row, err := db.Query("SELECT itemID, amount FROM stockhome.history WHERE action = 0 AND date BETWEEN (?) AND (?)", startDate, endDate)
 
 	if err != nil {
 		fmt.Print(err)
@@ -272,10 +212,9 @@ func MostWithDate(start string, Wg *sync.WaitGroup) string {
 	return txt.String()
 }
 
-func WithTime(Wg *sync.WaitGroup) string {
-	defer Wg.Done()
+func WithTime() string {
 	var txt strings.Builder
-	row, err := db.Query("SELECT time, amount FROM history WHERE action = 0")
+	row, err := db.Query("SELECT time, amount FROM stockhome.history WHERE action = 0")
 
 	if err != nil {
 		fmt.Print(err)
@@ -311,10 +250,9 @@ func WithTime(Wg *sync.WaitGroup) string {
 	return txt.String()
 }
 
-func WithDate(Wg *sync.WaitGroup) string {
-	defer Wg.Done()
+func WithDate() string {
 	var txt strings.Builder
-	row, err := db.Query("SELECT date, amount FROM history WHERE action = 0")
+	row, err := db.Query("SELECT date, amount FROM stockhome.history WHERE action = 0")
 
 	if err != nil {
 		fmt.Print(err)
@@ -341,7 +279,7 @@ func WithDate(Wg *sync.WaitGroup) string {
 	for date := range withMap {
 		withSort = append(withSort, date)
 	}
-	sort.Strings(withSort)
+	sort.Sort(sort.Reverse(sort.StringSlice(withSort)))
 
 	var i int = 0
 	for _, date := range withSort {
@@ -355,30 +293,99 @@ func WithDate(Wg *sync.WaitGroup) string {
 	return txt.String()
 }
 
-func pulldb(con net.Conn, date string) {
-	wgdb.Add(1)
-	defer wgdb.Done()
-	// var err error
-	// db, err = sql.Open("mysql", "root:pinkponk@tcp(209.97.170.50:3306)/stockhome")
-	// if err != nil {
-	// 	fmt.Println("Error: Cannot open database")
-	// }
-	// defer db.Close()
-	// buf := bytes.NewBuffer(make([]byte, 0))
-	col := []byte("userID,itemID,amount,date,time")
-	// buf.Write(col)
-	con.Write(col)
-	// str := "userID,itemID,amount,date,time"
-	// fmt.Println(Date)
-	// Get data from startDate to endDate
-	startDate := date + "-01" //2021-02-01
-	endDate := date + "-31"   //2021-02-31
-	fmt.Println(startDate)
-	row, err := db.Query("SELECT userID, itemID, amount, date, time FROM history WHERE date BETWEEN (?) AND (?) ORDER BY date ASC, time ASC", startDate, endDate)
+func add(userID string, itemID string, itemAmount string) string {
+
+	var checkID, stock int
+	var statement string
+	itemID2, _ := strconv.Atoi(itemID)
+	amount, _ := strconv.Atoi(itemAmount)
+
+	check := db.QueryRow("SELECT itemID FROM stock WHERE itemID = (?)", itemID2).Scan(&checkID)
+
+	if check != nil {
+		_, err := db.Exec("INSERT INTO stock VALUES (?, ?)", itemID, amount)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		statement = fmt.Sprint("Added %d to database (%d units) | Item in Stock: %d\n", itemID, amount, amount)
+
+	} else {
+		check := db.QueryRow("SELECT itemID, amount FROM stock WHERE itemID = (?)", itemID).Scan(&checkID, &stock)
+
+		if check != nil {
+			fmt.Println("Error: Item does not exist in database")
+		} else {
+			_, err := db.Exec("UPDATE stock SET amount = (?) WHERE itemID = (?)", stock+amount, itemID2)
+
+			if err != nil {
+				fmt.Println(err)
+				return "error happended in addExist"
+			}
+			statement = fmt.Sprintf("Added %d to database (%d units) | Item in Stock: %d\n", itemID, amount, stock+amount)
+		}
+	}
+	fmt.Println(statement)
+	return statement
+}
+
+func withdraw(userID string, itemID string, itemAmount string) string {
+
+	var checkID, stock int
+	var statement string
+	itemID2, _ := strconv.Atoi(itemID)
+	amount, _ := strconv.Atoi(itemAmount)
+
+	check := db.QueryRow("SELECT itemID, amount FROM stock WHERE itemID = (?)", itemID2).Scan(&checkID, &stock)
+
+	if check != nil {
+		fmt.Println("Error: No item in stock")
+
+	} else if amount > stock {
+		fmt.Println("Error: Amount exceeds stock")
+
+	} else {
+		_, err := db.Exec("UPDATE stock SET amount = (?) WHERE itemID = (?)", stock-amount, itemID2)
+
+		if err != nil {
+			fmt.Printf("\n")
+		}
+		statement = fmt.Sprintf("Withdrawn %d from database (%d units) | Item in Stock: %d\n", itemID, amount, stock-amount)
+	}
+	fmt.Println(statement)
+
+	return statement
+}
+
+func getItemAmount(itemID string) string {
+
+	row, err := db.Query("SELECT amount FROM stock WHERE itemID = (?)", itemID)
+
 	if err != nil {
 		fmt.Print(err)
 	}
 
+	var amount int
+	for row.Next() {
+		err = row.Scan(&itemID, &amount)
+	}
+	a := itemID + "-" + strconv.Itoa(amount)
+	fmt.Println(a)
+	return a
+}
+
+func his(year string, month string) []byte {
+	Date := year + "-" + month
+	buf := bytes.NewBuffer(make([]byte, 0))
+	col := []byte("userID,itemID,amount,date,time")
+	buf.Write(col)
+	startDate := Date + "-01" //2021-02-01
+	endDate := Date + "-31"   //2021-02-31
+
+	row, err := db.Query("SELECT userID, itemID, amount, date, time FROM history WHERE date BETWEEN (?) AND (?) ORDER BY date ASC, time ASC", startDate, endDate)
+	if err != nil {
+		fmt.Print(err)
+	}
 	// Slice each row
 	for row.Next() {
 		var userID, itemID, amount int
@@ -389,73 +396,10 @@ func pulldb(con net.Conn, date string) {
 		}
 		// Write each line
 		line := []byte("\n" + strconv.Itoa(userID) + "," + strconv.Itoa(itemID) + "," + strconv.Itoa(amount) + "," + date + "," + time)
-		// str += ("\n" + strconv.Itoa(userID) + "," + strconv.Itoa(itemID) + "," + strconv.Itoa(amount) + "," + date + "," + time)
-		// buf.Write(line)
-		con.Write(line)
+		buf.Write(line)
 	}
-	// con.Write(buf.Bytes())
-	con.Write([]byte("."))
+	row.Close()
 
-}
-
-func add(userID string, itemID string, itemAmount string) string {
-	wgadd.Add(1)
-	defer wgadd.Done()
-	cs, err := net.Dial("tcp", "143.198.195.15:5003")
-	if err != nil {
-		fmt.Println(err)
-		cs.Close()
-		return "nil" + "*" + "no" + "\n"
-	}
-	defer cs.Close()
-	cs.Write([]byte("add:" + itemID + "-" + itemAmount + "-" + userID + "\n"))
-	val, err := bufio.NewReader(cs).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return "nil" + "*" + "no" + "\n"
-	}
-	fmt.Println(val)
-
-	return val
-}
-
-func withdraw(userID string, itemID string, itemAmount string) string {
-	wgwd.Add(1)
-	defer wgwd.Done()
-	cs, err := net.Dial("tcp", "143.198.195.15:5003")
-	if err != nil {
-		fmt.Println(err)
-		cs.Close()
-		return "nil" + "*" + "no" + "\n"
-	}
-	defer cs.Close()
-	cs.Write([]byte("wd:" + itemID + "-" + itemAmount + "-" + userID + "\n"))
-	val, err := bufio.NewReader(cs).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return "nil" + "*" + "no" + "\n"
-	}
-	fmt.Println(val)
-
-	return val
-}
-
-func getItemAmount(itemID string) string {
-	wgget.Add(1)
-	defer wgget.Done()
-	cs, err := net.Dial("tcp", "143.198.195.15:5003")
-	if err != nil {
-		fmt.Println(err)
-		cs.Close()
-		return "nil" + "*" + "no" + "\n"
-	}
-	defer cs.Close()
-	cs.Write([]byte("get:" + itemID + "\n"))
-	val, err := bufio.NewReader(cs).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return "nil" + "*" + "no" + "\n"
-	}
-	fmt.Println(val)
-	return val
+	// Data that will be sent
+	return buf.Bytes()
 }
