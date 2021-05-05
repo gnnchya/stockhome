eb42aa365c3a840a29a8619c1fe0beff1bc41c07
thachawit <43896511+thachawit@users.noreply.github.com>
2021-05-05 21:11:08 +0700

    Update tcpShield.go

--- new/tcpShield.go
+++ new/tcpShield.go
@@ -17,9 +17,9 @@ import (
 var myCache LRU
 var mutex = &sync.Mutex{}
 
-var sadd = make(chan bool, 500)
-var swd = make(chan bool, 500)
-var sget = make(chan bool, 500)
+var sadd = make(chan bool, 1000)
+var swd = make(chan bool, 1000)
+var sget = make(chan bool, 1000)
 
 func main() {
 
