fdbb9ca27f625203a40a9828b37773863206b632
thachawit <43896511+thachawit@users.noreply.github.com>
2021-05-05 22:46:35 +0700

    Update tcpShield.go

--- new/tcpShield.go
+++ new/tcpShield.go
@@ -135,15 +135,11 @@ func send(con net.Conn, msg string) {
 func GetAmount(itemID int) string {
 
 	defer func() { <-sget }()
-	row, err := Db.Query("SELECT itemID, amount FROM stock WHERE itemID = (?)", itemID)
-
-	if err != nil {
-		fmt.Print(err)
-	}
-
 	var amount int
-	for row.Next() {
-		err = row.Scan(&itemID, &amount)
+	check := Db.QueryRow("SELECT amount FROM stock WHERE itemID = (?)", itemID).Scan(&amount)
+
+	if check != nil {
+		fmt.Print(err)
 	}
 	return strconv.Itoa(amount)
 }
