8633d5d23411d603b68a70cfabab48afa9406c5e
pondnarawich <pondnarawich@gmail.com>
2021-05-06 03:03:10 +0700

    Update tcpdb.go

--- baseline_sys/tcpdb.go
+++ baseline_sys/tcpdb.go
@@ -109,7 +109,7 @@ func add(userID string, itemID string, itemAmount string) string {
 		}
 	}
 	fmt.Println(statement)
-	return itemID + "-" + strconv.Itoa(stock-amount)
+	return itemID + "-" + strconv.Itoa(stock+amount)
 }
 
 func withdraw(userID string, itemID string, itemAmount string) string {
