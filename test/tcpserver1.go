6150a66a5c5b507ca35e1c1e959dbf7ed4788b93
pondnarawich <pondnarawich@gmail.com>
2021-05-06 14:56:49 +0700

    Update tcpserver1.go

--- test/tcpserver1.go
+++ test/tcpserver1.go
@@ -52,7 +52,7 @@ func rec(con net.Conn) {
 			date[0] = strings.TrimSpace(date[0])
 			date[1] = strings.TrimSpace(date[1])
 			date[2] = strings.TrimSpace(date[2])
-			ana := analysis(date[0], date[1], date[2])
+			ana := Analysis(date[0], date[1], date[2])
 			send(con, ana)
 		case "add":
 			id := strings.Split(msg[1], "-")
