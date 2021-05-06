dce871cb121152faae30486c41a9b04ab70131cc
pondnarawich <pondnarawich@gmail.com>
2021-05-07 00:02:32 +0700

    Update tcpShield.go

--- new/tcpShield.go
+++ new/tcpShield.go
@@ -390,6 +390,7 @@ func (l *LRU) Input(itemID int, ItemAmount int) (int, bool) {
 		}
 		// itemamount  เป็นลบแล้วไม่ found
 		fmt.Println("-----MISS-----")
+		sget <- true
 		GetAmountVal, _ := strconv.Atoi(GetAmount(itemID))
 		if GetAmountVal+ItemAmount < 0 {
 			fmt.Print("ItemID: %#v  cannot be withdraw!!, Negative Value", itemID)
