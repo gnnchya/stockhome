39c839ab2d794a1e5ab1ab72c55fe1e0c3bd25e9
pondnarawich <pondnarawich@gmail.com>
2021-05-07 18:55:48 +0700

    Update tcpserver1.go

--- test/tcpserver1.go
+++ test/tcpserver1.go
@@ -91,7 +91,7 @@ func send(con net.Conn, msg string) {
 
 func sendhis(con net.Conn, msg []byte) {
 	temp := append([]byte("Server: "), msg...)
-	temp1 := append(temp, []byte("`")...)
+	temp1 := append(temp, []byte("**`")...)
 	con.Write(temp1)
 
 }
