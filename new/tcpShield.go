6d6617080b06345999724c5f33832124bb1b1f18
thachawit <43896511+thachawit@users.noreply.github.com>
2021-05-07 22:06:01 +0700

    Update tcpShield.go

--- new/tcpShield.go
+++ new/tcpShield.go
@@ -24,7 +24,7 @@ var mwd sync.Mutex
 var mget sync.Mutex
 
 func main() {
-	myCache.InitLRU(1000)
+	myCache.InitLRU(2000)
 	connect, err := net.Listen("tcp4", "143.198.195.15:5003")
 	if err != nil {
 		fmt.Println(err)
