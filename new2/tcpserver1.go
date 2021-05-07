6e767c813649cfefbc2871a23b4e2630e436ef02
flukekd <57992223+flukekd@users.noreply.github.com>
2021-05-07 12:38:24 +0700

    Update tcpserver1.go

--- new2/tcpserver1.go
+++ new2/tcpserver1.go
@@ -43,6 +43,7 @@ func main() {
 }
 
 func rec(con net.Conn) {
+	var ana *string
 	for {
 		data, err := bufio.NewReader(con).ReadString('\n')
 		if err != nil {
@@ -60,7 +61,6 @@ func rec(con net.Conn) {
 			date[0] = strings.TrimSpace(date[0])
 			date[1] = strings.TrimSpace(date[1])
 			date[2] = strings.TrimSpace(date[2])
-			var ana *string
 			*ana = analysis(date[0], date[1], date[2])
 			send(con, *ana)
 			runtime.GC()
